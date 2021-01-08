fa2b8be4ff043607314284c899fa0537d952b93f
Sam Jordan <sjordan@gsspos.co.uk>
2021-01-08 13:36:44 +0000

    Add project files.

---
+++ s/SwipeViewBreaksNonSwipeTabbedPage/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace SwipeViewBreaksNonSwipeTabbedPage.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+            Items.Add("woo!");
+        }
+
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+    }
+}
